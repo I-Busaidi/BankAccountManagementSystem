f3a78aace32ce38c72c3e7c8a13d25de71fc13ed
Ibrahim Al Busaidi <i.s.busaidi@gmail.com>
2024-09-25 15:08:31 +0400

    Worked on Bank Class.

--- Bank.cs
+++ Bank.cs
@@ -6,23 +6,54 @@ using System.Threading.Tasks;
 
 namespace BankAccountManagementSystem
 {
-    internal class Bank
+    public class Bank
     {
         private List<(string AccNumber, string AccHolderName, double Balance)> BankAccounts = new List<(string AccNumber, string AccHolderName, double Balance)>();
 
-        public void AddNewAccount()
+        public bool CheckExistingAcc(string AccNumber, string AccHolderName)
         {
+            bool AccExists = false;
+            for (int i = 0; i < BankAccounts.Count; i++)
+            {
+                if ((BankAccounts[i].AccNumber.Trim() == AccNumber.Trim()) || (BankAccounts[i].AccHolderName.Trim().ToLower() == AccHolderName.Trim().ToLower()))
+                {
+                    AccExists = true;
+                }
+            }
+            return AccExists;
+        }
 
+        public void AddNewAccount(string AccNumber, string AccHolderName, double Balance)
+        {
+            BankAccounts.Add((AccNumber, AccHolderName, Balance));
         }
 
         public void GetAccByNumber(string AccNumber)
         {
-
+            for (int i = 0;i < BankAccounts.Count;i++)
+            {
+                if (BankAccounts[i].AccNumber.Trim() == AccNumber.Trim())
+                {
+                    Console.WriteLine($"Bank Account Number: {BankAccounts[i].AccNumber}\n" +
+                        $"Bank Account Holder Name: {BankAccounts[i].AccHolderName}.\n" +
+                        $"Bank Account Balance: ${BankAccounts[i].Balance}\n");
+                    break;
+                }
+            }
         }
 
         public void DisplayAllAccounts()
         {
-
+            StringBuilder sb = new StringBuilder();
+            string border = new string('-', 100);
+            sb.AppendLine($"{"Account Number", 30} | {"Account Holder Name", 30} | {"Balance", 30}");
+            for (int i = 0; i < BankAccounts.Count ; i++)
+            {
+                sb.AppendLine($"{"",30} | {"",30} | {"",30}");
+                sb.AppendLine($"{BankAccounts[i].AccNumber,30} | {BankAccounts[i].AccHolderName,30} | {BankAccounts[i].Balance,30}");
+            }
+            Console.WriteLine( sb.ToString() );
+            sb.Clear();
         }
     }
 }
